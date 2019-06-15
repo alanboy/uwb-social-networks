513e793be6b5ba3c333b8d08e912b232ee4f7609
Alan Gonzalez <alanboy@alanboy.net>
2019-06-15 09:49:27 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const qs = require('querystring');
+var FB = require('fb');
+var app = express();
+
+var server = app.listen(80);
+
+function showFriends(request, response) {
+
+    FB.setAccessToken(at);
+    FB.api('me/friends', function (res) {
+      if(!res || res.error) {
+         console.log(!res ? 'error occurred' : res.error);
+         response.send(res.error);
+         return;
+      }
+      response.send(res);
+    });
+}
+
+app.get('/showFriends', showFriends);
+app.use(express.static('.'));
+
